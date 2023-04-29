b1b33594ec32aefbd85c1809978eca509e47ae2e
Imran691 <mimran691@gmail.com>
2023-04-29 10:22:36 +0500

    first commit

---
+++ src/app/api/crud/route.ts
@@ -0,0 +1,70 @@
+// Functions are named after the methods used to call APIs. In this way whichever method is used, same function is called accordingly.
+// for each end point we ceate CRUD system.
+
+import { NextRequest, NextResponse } from "next/server";
+
+// GET method returns data. Here it returns an object.
+// when we call a function with API, All data is received in "request"
+export async function GET(request: NextRequest) {
+    return NextResponse.json({
+      From: "Zia",
+      Message: "Greetings from Pakistan",
+      RequestType: "GET"
+    });
+}
+
+
+// clients send data in "request"
+// POST method is used to create data in data base.
+// to create data we need to send data. In postman we send data in request body, in "raw" in "json" format.
+// This data is recieved in "request" and we convert this data in json using "request.json" method.
+// 
+export async function POST(request: NextRequest) {
+  const req = await request.json();     // we use "await" because it is a promise.
+  if(req.name){         // If request body object contains "name" propoety
+    return NextResponse.json({
+      To: req.name,
+      Message: "All well here",
+      RequestType: "POST"
+    });
+  }
+  else {
+    return new NextResponse('Please include your name in the POST request');
+  }
+}
+
+
+//PUT is used to update data in data base.
+// We send body with PUT as well containing data that is to be updated.
+export async function PUT(request: NextRequest) {
+  const req = await request.json();
+  if(req.name){
+    return NextResponse.json({
+      To: "Zia",
+      Message: "This is a updated greeting",
+      RequestType: "PUT"
+    });
+  }
+  else {
+    return new NextResponse('Please include your name in the PUT request');
+  }
+}
+
+
+// DELETE method is used to delete data from data base.
+// We need not to attach data in the request body.
+// We only need to send the ID of data to be deleted.
+// We add the ID of data to be deleted in request URL as   "URL/ID" or as query parameter with request URl as "URl?id=1"
+export async function DELETE(request: NextRequest) {
+  // const req = await request.json();
+  // if(req.name){
+    return NextResponse.json({
+      To: "zia",
+      Message: "I have deleted the greetings",
+      RequestType: "DELETE"
+    });
+  }
+//   else {
+//     return new NextResponse('Please include your name in the DELETE request');
+//   }
+// }
--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,30 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+// for API we need separete folder in app directory to avoid path conflict between page.tsx & route.ts.
+// we can't create route.ts in app directly, because when we run our app page.tsx is called by default.
+// Next.js will throw an error.
+
+
+// we write code in the form of a function. API calls this function.
+// API collects request from client and takes it to server and collects response from server and takes it to client.
+// we name these functions afetr API methods i.e. "GET", "POST", "PATCH" etc. SO that when one calls GET method, this function is called.
+
+// when someone calls this API we will recieve a parameter. Conventionally we name it "request". An object will be recieved in "request" and can be some cookies, header or URL. We assign this parameter type "NextRequest"
+
+//We can add some data in URL after ? mark in the form of a variable, this is called query parameter.
+// this parameter is received in "request" parameter.
+// and extracted by using the method "request.nextUrl"
+// ".has" method is used to extraxt query parameters from the request
+
+import { NextRequest, NextResponse } from "next/server"
+
+export const GET = async (request:NextRequest) => {
+  
+  const url = request.nextUrl;  // to add query parameters with request URL.
+  if(url.searchParams.has("name")){    // to check if url has a query parameter named "name"
+    const name = url.searchParams.get("name");  // to extract the value of query parameter by using "get" method.
+    
+    return NextResponse.json({message:"Hello, from: " + name});   // ".json" method to return the response in json format
+  }
+  else {        // for error handling
+    return new NextResponse('Please send your name in search parameter "name"');
+  }
 }
